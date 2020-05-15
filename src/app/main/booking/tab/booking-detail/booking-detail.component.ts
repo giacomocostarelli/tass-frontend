cd3bd014d2b48d573203e89683062e1ba4cfb2bf
Giuseppe Biondi <giuv360@hotmail.it>
2020-05-15 17:12:51 +0200

    2

--- src/app/main/booking/tab/booking-detail/booking-detail.component.ts
+++ src/app/main/booking/tab/booking-detail/booking-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-booking-detail',
+  selector: 'booking-detail',
   templateUrl: './booking-detail.component.html',
   styleUrls: ['./booking-detail.component.scss']
 })
--- src/app/main/booking/tab/tab.component.ts
+++ src/app/main/booking/tab/tab.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-tab',
+  selector: 'tab1',
   templateUrl: './tab.component.html',
   styleUrls: ['./tab.component.scss']
 })
 export class TabComponent implements OnInit {
-
+    watchingDetails = false;
   constructor() { }
 
   ngOnInit() {
